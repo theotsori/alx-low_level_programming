e4e5981f24bc2231569db8bc7a65cd72848e2125
theotsori <theodoretsori@gmail.com>
2022-10-12 03:01:14 -0700

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,22 +10,13 @@
  * Return: 0
  *
  */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
 
-#ifdef _DOG_T_H_
-#define _DOG_T_H_
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 } dog_t;
-#endif
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
