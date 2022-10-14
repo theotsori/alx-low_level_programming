43e8f2eed8e65f271729e775de495109504bac23
theotsori <theodoretsori@gmail.com>
2022-10-14 00:45:38 -0700

    Working calc

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add sum of 2 ints
+ * @a: int1
+ * @b: int2
+ *
+ * Return: rel
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference of 2 ints
+ * @a: int1
+ * @b: int2
+ *
+ * Return: rel
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product of 2 ints
+ * @a: int1
+ * @b: int2
+ *
+ * Return: rel
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division of 2 ints
+ * @a: int1
+ * @b: int2
+ *
+ * Return: rel
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo of 2 ints
+ * @a: int1
+ * @b: int2
+ *
+ * Return: rel
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
