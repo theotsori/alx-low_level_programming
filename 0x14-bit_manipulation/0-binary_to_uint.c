db149a1a81a3fe5edd8c04d2158628b6397b6add
theotsori <theodoretsori@gmail.com>
2022-11-01 18:57:46 +0000

    Bitwise manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,23 +3,24 @@
 /**
  * binary_to_uint - function that converts a binary num
  * to an unsigned int.
- * b - const char
+ * @b: const char
  *
  * Return: converted num b is null  if not num
  */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
-	int i = 0;
 
-	if (b == NULL)
-	return 0;
-
-	while (b[i] == 48 || b[i] == 49)
+	if (!b)
+		return (0);
+	while (*b)
 	{
+		if (*b != 48 &&  *b != 49)
+			return (0);
 		num <<= 1;
-		num += b[i] - 48;
-		i++;
+		if (*b & 1)
+			num += 1;
+		b++;
 	}
-	return num;
+	return (num);
 }
