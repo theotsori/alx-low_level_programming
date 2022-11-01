a12e03c84e3498d22fe671b409ba43b2cb6776a8
theotsori <theodoretsori@gmail.com>
2022-11-01 18:43:47 +0000

    Print bit fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,14 +10,15 @@
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
+	{
 		print_binary(n >> 1);
-	printf("%ld", n & 1);
+	}
+	if (n & 1)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
 }
-/*
-{
-	unsigned i;
-
-	for (i = 1 << 31; i > 0; i = i / 2)
-	(n & i) ? _putchar(49) : _putchar(48);
-}
-*/
