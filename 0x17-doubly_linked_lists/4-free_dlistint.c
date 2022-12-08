b0bada5529d94bbf32fc104b90877c8832b73d1e
theotsori <theodoretsori@gmail.com>
2022-12-08 13:50:53 +0300

    doubly list node free

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - function that frees a doubly list
+ * @head: head node
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *cur = head;
+
+	while (cur != NULL)
+	{
+		dlistint_t* next = cur->next;
+		free(cur);
+		cur = next;
+	}
+}
