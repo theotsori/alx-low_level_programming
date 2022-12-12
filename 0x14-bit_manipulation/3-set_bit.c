11bb4c007de070b237a3573a99350fe653311a91
theotsori <theodoretsori@gmail.com>
2022-12-12 10:13:59 +0300

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,10 +10,13 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int mask = 1;
+
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	*n ^= (1 << index);
+	mask = mask << index;
+	*n = *n | mask;
 
 	return (1);
 }
