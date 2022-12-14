c35a4587f90096cb6c07e08c062bc93ede6ecb5a
theotsori <theodoretsori@gmail.com>
2022-12-14 13:31:18 +0300

    bash exe

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-# Get all the C files in the current directory
-files=$(ls *.c)
-
-# Compile the C files into object files
-for file in $files
-do
-  gcc -c $file
-done
-
-# Create the dynamic library from the object files
+gcc -fPIC -c *.c
 gcc -shared -o liball.so *.o
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
