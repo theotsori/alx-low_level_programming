68f48e99ef36a6a6c168d7e003c98a4e46403433
theotsori <theodoretsori@gmail.com>
2023-01-07 15:46:06 +0300

    >>>ht['betty']='cool'

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,47 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function that adds an element to the hash table
+ * @ht: first node
+ * @key: is key
+ * @value: is value
+ * Return: 1 if success else 0
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node, *temp;
+
+	if (!ht || !key || !*key || !value)
+	{
+		return (0);
+	}
+
+	index = key_index((const unsigned char *) key, ht->size);
+	temp = ht->array[index];
+
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			return (1);
+		}
+		temp = temp->next;
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
+
+	if (!new_node)
+	{
+		return (0);
+	}
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+}
